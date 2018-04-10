953db2a55f8e30a7e04674c7024bdf362896ac56
tyamasaki <tyamasaki@fortinet.com>
2018-04-10 11:13:14 +0900

    Windows用 起動バッチ コメント追加 v3

--- webserver_turn-on.bat
+++ webserver_turn-on.bat
@@ -8,6 +8,6 @@ rem ===================================
 rem PHP Web Server Turn On
 rem ===================================
 cd /d %~dp0
-del /Q app\cache\prod
+rem del /Q app\cache\prod
 php app/console server:run --env=prod 0.0.0.0:80
 pause
